2f1485e7054a92c3fd00f460f843cf25f2247d10
Cherif hassan <nousradinecherifhassane@gmail.com>
2022-10-12 03:41:32 +0200

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Shazam App',
       theme: ThemeData(
         primaryColor: Color(0xffE38B29),
